5e91d369af433c932e20acadc922df85787e2bfa
Ibrahim Mustapha Oyeyemi <67954703+mustaphatg@users.noreply.github.com>
2022-11-21 05:06:12 +0100

    updated the api endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,10 @@ import axios from "axios"
 
 
 
-var url = "https://nairaland-20.herokuapp.com/api/"
+// var url = "https://nairaland-20.herokuapp.com/api/"
   //url = "http://localhost:8000/api/"
+
+var url = "https://nairaland-backend.fly.dev/api/"
 
 export function getHome(){
      console.log( "Getting Home" )
